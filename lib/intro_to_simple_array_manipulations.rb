4d34e46161baea1111696dd6d491a71290f7543c
Aniece Thompson <aniecet@yahoo.com>
2019-09-26 19:55:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,15 +32,15 @@ def using_pop(array)
   end 
   
   def using_uniq(array)
-   
+   array.uniq
   end
   
   def using_flatten(array)
-    
+    array.flatten
   end
   
-  def using_delete
-    
+  def using_delete(array,string)
+    array.delete
   end
   
   def using_delete_at
