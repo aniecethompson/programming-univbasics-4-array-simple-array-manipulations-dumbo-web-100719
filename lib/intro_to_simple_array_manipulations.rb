7433dabc0840a50c1e6741f7369a26ba69b8c969
Aniece Thompson <aniecet@yahoo.com>
2019-09-26 18:45:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,59 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift (array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  @new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift(@new_neighborhood)
+end
+
+def using_pop(array)
+   @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+   @continents.pop
+ end
+ 
+ def pop_with_args(array)
+   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+   @dog_breeds.pop(2)
+ end
+   
+  def using_shift(array)
+   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+   @my_favorite_cities.shift
+ end
+ 
+ def shift_with_args(array)
+   @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+   @ice_cream_brands.shift(2)
+ end
+ 
+ def using_concat(*array)
+    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    @my_favorite_things.concat(more_favs)
+  end
+  
+  def using_insert(array, argument)
+    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
+    @list_of_programming_languages.insert(@another_language)
+  end 
+  
+  def using_uniq
+    
+  end
+  
+  def using_flatten
+    
+  end
+  
+  def using_delete
+    
+  end
+  
+  def using_delete_at
+
+end
