5517f68e9063175eab86df9ed710409e4650c4e9
Aniece Thompson <aniecet@yahoo.com>
2019-09-26 20:21:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,12 +24,12 @@ def using_pop(array)
  
  def using_concat(*array)
    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["sports cars", "flatiron school"]
-  @my_favorite_things.concat(more_favs)
+   more_favs = ["sports cars", "flatiron school"]
+   @my_favorite_things.concat(more_favs)
   end
   
   def using_insert(array, string)
-  array.insert(4,string)
+    array.insert(4,string)
   end 
   
   def using_uniq(array)
