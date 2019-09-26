b9dcc14e7529ff81658910c61df70123398d50e8
Aniece Thompson <aniecet@yahoo.com>
2019-09-26 19:36:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,5 @@
 def using_push(array, string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
-  colors_in_the_rainbow.push(next_color)
+  array.push(color)
 end
 
 def using_unshift(array, string)
@@ -9,28 +7,22 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-   @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-   @continents.pop
+   array.pop
  end
  
  def pop_with_args(array)
-   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-   @dog_breeds.pop(2)
+   array.pop(2)
  end
    
   def using_shift(array)
-   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-   @my_favorite_cities.shift
+   array.shift
  end
  
  def shift_with_args(array)
-   @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-   @ice_cream_brands.shift(2)
+   array.shift(2)
  end
  
  def using_concat(*array)
-    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["sports cars", "flatiron school"]
     array.concat(array)
   end
   
