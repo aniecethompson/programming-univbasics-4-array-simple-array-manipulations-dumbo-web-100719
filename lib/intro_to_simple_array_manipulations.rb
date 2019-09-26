3a5e76075563b7804be8980c19dadc192954dc5c
Aniece Thompson <aniecet@yahoo.com>
2019-09-26 20:21:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,9 +23,9 @@ def using_pop(array)
  end
  
  def using_concat(*array)
-  @my_favorite_things = arr1
-  more_favs = arr2
-  arr1.concat(arr2)
+   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+  @my_favorite_things.concat(more_favs)
   end
   
   def using_insert(array, string)
